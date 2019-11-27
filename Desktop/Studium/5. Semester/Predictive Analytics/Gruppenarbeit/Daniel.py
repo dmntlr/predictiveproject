63e526d12c65438ee9da8ddcb7b8cf37f73628f8
Flamejunkey <58259511+Flamejunkey@users.noreply.github.com>
2019-11-27 14:57:21 +0100

    Mit na_values

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/Daniel.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/Daniel.py
@@ -1,24 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#CSV Datei lesen, es wird beim Komma in eine neue Spalte aufgeteilt
-data=pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
+missing_values = ["na","--"]
+# read csv into a dataframe
+propdata=pd.read_csv("19.06.20_travels_Frankfurt.csv", sep=',' ,  na_values= missing_values)
 
-#Zugriff auf jede Spalte
-TAA = data["TAA"]
-TA = data["TA"]
-TIN = data["TIN"]
-TIR = data["TIR"]
-TSI = data["TSI"]
-TIM = data["TIM"]
-TIL = data["TIL"]
-TIRE = data["TIRE"]
-TIP = data["TIP"]
-TIT = data["TIT"]
-TID = data["TID"]
-TSC = data["TSC"]
-TAC = data["TAc"]
-
-#Ausgabe jeder Zeile für eine Spalte
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(TAA)
+print(propdata)
