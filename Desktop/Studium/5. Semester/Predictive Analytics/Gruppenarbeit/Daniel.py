d81b2ae6f5b9edf0573e03efad701f4e77816f48
Daniel Mantler <daniel-mantler@live.de>
2019-11-27 14:43:12 +0100

    data seperated

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/Daniel.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/Daniel.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-missing_values = ["na","--"]
-# read csv into a dataframe
-propdata=pd.read_csv("data.csv", na_values= missing_values)
+data=pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
 
-plt.plot(propdata)
-plt.show()
+print(data)
