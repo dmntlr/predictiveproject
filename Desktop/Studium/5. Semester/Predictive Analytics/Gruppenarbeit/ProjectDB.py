b50e1104ac66ceb8be4b35ab0af5ff7014d8c21e
Daniel Mantler <daniel-mantler@live.de>
2019-11-27 17:43:06 +0100

    Züge sortiert nach verpätung

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from operator import itemgetter
+import numpy as np
 #CSV Datei lesen, es wird beim Komma in eine neue Spalte aufgeteilt
 data=pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
 
@@ -21,21 +22,25 @@ TAC = data["TAc"]
 
 #Ausgabe jeder Zeile für eine Spalte
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(TA)
+      print(TAC)
+
 
 
 #Analyse der verspäteten Züge für die ersten 100 Zeilen
 #löscht Zeilen mit nan-Werten
 dataset = data.dropna()
+#Sortieren nach verspätung
+dataset = dataset.sort_values(by=['TAc'], ascending=False)
 
-X = dataset.iloc[0:100, 2:3].values
-y = dataset.iloc[0:100, 12:13].values
+X = dataset.iloc[:, 2:3].values.ravel()
+y = dataset.iloc[:, 12:13].values.ravel()
 
 
-xx = X.ravel()
-yy = y.ravel()
+#Daten bereinigen da auch hohe negative Zahlen als Fehlzeiten drinne sind Da für TAC ja TA - TIT gerechnet wird
+y = [0 if x<0 else x for x in y]
 
-plt.bar(xx, yy, color='red')
+plt.figure(figsize=(40, 4))
+plt.bar(X,y)
 plt.xticks(rotation='vertical')
-#plt.savefig("test.png")
+
 plt.show()
