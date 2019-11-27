a342fd0781045c57281ec36b84d4ba1675f3569d
Keßler <KESSP2@de.softlab.net>
2019-11-27 19:54:23 +0100

    binäre Spalte "LATE" (Verspätung Ja/Nein) und Versuch plt.scatter

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from operator import itemgetter
 import numpy as np
 #CSV Datei lesen, es wird beim Komma in eine neue Spalte aufgeteilt
-data=pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
+data = pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
 
 #Zugriff auf jede Spalte
 TAA = data["TAA"]
@@ -29,7 +29,7 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
 #löscht Zeilen mit nan-Werten
 dataset = data.dropna()
 #Alle negativen Zahlen entfernen damit zu frühe abfahrtzeiten nicht gezählt werden
-dataset = dataset[(dataset.iloc[:,12:13] > 0).all(1)]
+dataset = dataset[(dataset.iloc[:, 12:13] > 0).all(1)]
 #Groupiert nach Zügen und summiert dabei die Zeiten auf
 dataset = dataset.groupby('TIN').agg({'TAc' : 'sum'}).reset_index()
 #Sortiert nach Zeiten
@@ -40,6 +40,28 @@ X = dataset.iloc[:, 0:1].values.ravel()
 y = dataset.iloc[:, 1:2].values.ravel()
 
 plt.figure(figsize=(40, 4))
-plt.bar(X,y)
+plt.bar(X, y)
+plt.xticks(rotation='vertical')
+plt.show()
+
+#Wahrscheinlichkeit, dass ein bestimmter Zug zu spät kommt
+
+data1 = dataset.loc[:, ['TIN', 'TAc']]
+data1['TAc'] = [0 if x < 0 else x for x in y]
+
+#binäre Spalte 'LATE' (1=Verspätung, 0=keine Verspätung)
+late = []
+for row in data1['TAc']:
+      if row > 0:
+         late.append(1)
+      else:
+         late.append(0)
+
+data1['LATE'] = late
+
+X = data1.iloc[:, 0:1].values.ravel()
+y = data1.iloc[:, 2:3].values.ravel()
+
+plt.scatter(X, y)
 plt.xticks(rotation='vertical')
 plt.show()
