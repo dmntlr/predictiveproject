16369e1d46590369beb6f3d4163e3c2ee109df56
Keßler <KESSP2@de.softlab.net>
2019-11-28 15:22:37 +0100

    Logistische Regression mit Uhrzeit und Verspätung

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from operator import itemgetter
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 #CSV Datei lesen, es wird beim Komma in eine neue Spalte aufgeteilt
 missing_values = ["na", "", " Es verkehrt"]
 data = pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',', na_values=missing_values)
@@ -22,8 +25,8 @@ TSC = data["TSC"]
 TAC = data["TAc"]
 
 #Ausgabe jeder Zeile für eine Spalte
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-      print(TIN)
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+     # print(TIN)
 
 
 
@@ -33,13 +36,13 @@ dataset = data.dropna()
 #Alle positiven Zahlen in einen neuen Datensatz hinzufügen, damit die negativen nicht gezählt werden.
 dataset = dataset[(dataset.iloc[:, 12:13] > 0).all(1)]
 #Groupiert nach Zügen und summiert dabei die Zeiten auf
-dataset = dataset.groupby('TIN').agg({'TAc' : 'sum'}).reset_index()
+data2 = dataset.groupby('TIN').agg({'TAc' : 'sum'}).reset_index()
 #Sortiert nach Zeiten
-dataset = dataset.sort_values(by=['TAc'], ascending=False)
+data2 = data2.sort_values(by=['TAc'], ascending=False)
 
 
-X = dataset.iloc[:, 0:1].values.ravel()
-y = dataset.iloc[:, 1:2].values.ravel()
+X = data2.iloc[:, 0:1].values.ravel()
+y = data2.iloc[:, 1:2].values.ravel()
 
 plt.figure(figsize=(40, 4))
 plt.bar(X, y)
@@ -48,27 +51,57 @@ plt.show()
 
 #Wahrscheinlichkeit, dass ein bestimmter Zug zu spät kommt
 
-data1 = dataset.loc[:, ['TIN', 'TAc']]
-data1['TAc'] = [0 if x < 0 else x for x in y]
+data1 = dataset.loc[:, ['TIN', 'TAc', 'TIT']]
+#data1['TAc'] = [0 if x < 0 else x for x in y]
 
 #binäre Spalte 'LATE' (1=Verspätung, 0=keine Verspätung)
 late = []
 cntlate = 0
 for row in data1['TAc']:
-      if row > 0:
+      if row > 1:
          late.append(1)
          cntlate+=1
       else:
          late.append(0)
 
 data1['LATE'] = late
+#nach Uhrzeiten sortiert
+data1 = data1.sort_values(by=['TIT'])
+#Uhrzeiten für Logistische Regression in Integer umgewandelt
+#Beispiel: 11:00 Uhr = 1100; 12:30 Uhr = 1230
+data1['TIT'] = data1['TIT'].str.replace(':', '').astype(int)
+X = data1.iloc[:, 2:3].values
+y = data1.iloc[:, 3:4].values
 
-X = data1.iloc[:, 0:1].values.ravel()
-y = data1.iloc[:, 2:3].values.ravel()
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+
+X_train_scaled, X_test_scaled, Y_train, Y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=0)
+
+X_train = scaler.inverse_transform(X_train_scaled) #transformiert die "scaled" Daten wieder zurück
+X_test = scaler.inverse_transform(X_test_scaled)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train_scaled, Y_train)
+y_pred = log_reg.predict(X_test_scaled)
 
 print("Es gibt " + cntlate.__str__() + " Züge die eine Verspätung haben.")
 
+#plt.figure(figsize=(50, 5))
+plt.scatter(X_train, Y_train, color='red', label='Training data')
+plt.xlabel('Uhrzeit')
+plt.ylabel('Verspätung (Ja/Nein)')
+X_train = np.sort(X_train[:, 0])
+X_train_sc = np.sort(X_train_scaled[:, 0])
+y_pred_proba = log_reg.predict_proba(X_train_sc.reshape(-1, 1))[:, 1]
+plt.plot(X_train, y_pred_proba, color='green', linewidth=3, label='Log Reg')
+plt.legend()
+plt.show()
 
-plt.scatter(X, y)
-plt.xticks(rotation='vertical')
+plt.scatter(X_test, Y_test, color='blue', label='Test data')
+plt.xlabel('Uhrzeit')
+plt.ylabel('Verspätung (Ja/Nein)')
+plt.plot(X_train, y_pred_proba, color='green', linewidth=3, label='Log Reg')
+plt.legend()
+plt.show()
 plt.show()
