bb7c8cbd830d711534898f2522aee6c15f7bd761
Flamejunkey <58259511+Flamejunkey@users.noreply.github.com>
2019-11-28 12:18:11 +0100

    Anzahl verspätete Züge + na.values

--- Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
+++ Desktop/Studium/5. Semester/Predictive Analytics/Gruppenarbeit/ProjectDB.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from operator import itemgetter
 import numpy as np
 #CSV Datei lesen, es wird beim Komma in eine neue Spalte aufgeteilt
-data = pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',')
+missing_values = ["na", "", " Es verkehrt"]
+data = pd.read_csv("19.06.20_travels_Frankfurt.csv",sep=',', na_values=missing_values)
 
 #Zugriff auf jede Spalte
 TAA = data["TAA"]
@@ -29,6 +30,7 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
 #löscht Zeilen mit nan-Werten
 dataset = data.dropna()
 #Alle negativen Zahlen entfernen damit zu frühe abfahrtzeiten nicht gezählt werden
+#Alle positiven Zahlen in einen neuen Datensatz hinzufügen, damit die negativen nicht gezählt werden.
 dataset = dataset[(dataset.iloc[:, 12:13] > 0).all(1)]
 #Groupiert nach Zügen und summiert dabei die Zeiten auf
 dataset = dataset.groupby('TIN').agg({'TAc' : 'sum'}).reset_index()
@@ -51,9 +53,11 @@ data1['TAc'] = [0 if x < 0 else x for x in y]
 
 #binäre Spalte 'LATE' (1=Verspätung, 0=keine Verspätung)
 late = []
+cntlate = 0
 for row in data1['TAc']:
       if row > 0:
          late.append(1)
+         cntlate+=1
       else:
          late.append(0)
 
@@ -61,6 +65,9 @@ data1['LATE'] = late
 
 X = data1.iloc[:, 0:1].values.ravel()
 y = data1.iloc[:, 2:3].values.ravel()
+
+print("Es gibt " + cntlate.__str__() + " Züge die eine Verspätung haben.")
+
 
 plt.scatter(X, y)
 plt.xticks(rotation='vertical')
